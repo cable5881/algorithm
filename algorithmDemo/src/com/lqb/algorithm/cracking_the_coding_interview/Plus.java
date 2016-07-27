741c4d34fb4e3244155c7e996b2e69b85c5faa66
cable5881 <412771478@qq.com>
2016-07-27 10:07:10 +0800

    test second commit

---
+++ algorithmDemo/src/com/lqb/algorithm/cracking_the_coding_interview/Plus.java
@@ -0,0 +1,29 @@
+package com.lqb.algorithm.cracking_the_coding_interview;
+
+import com.lqb.algorithm.offer.domain.ListNode;
+
+/**
+ * 有两个用链表表示的整数，每个结点包含一个数位。
+ * 这些数位是反向存放的，也就是个位排在链表的首部。
+ * 编写函数对这两个整数求和，并用链表形式返回结果。
+ * 给定两个链表ListNode* A，ListNode* B，请返回A+B的结果(ListNode*)。
+ * 测试样例：
+ * 
+ * {1,2,3},{3,2,1}
+ * 
+ * 返回：{4,4,4}
+ * @Author:JackBauer
+ * @Date:2016年7月27日
+ */
+public class Plus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public ListNode plusAB(ListNode a, ListNode b) {
+		return b;
+    }
+
+}
