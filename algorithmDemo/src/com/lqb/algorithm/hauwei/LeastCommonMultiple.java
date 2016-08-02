0c3d9e957c6d6a1a9eb602692af61584fae5f2e7
cable5881 <412771478@qq.com>
2016-08-02 11:23:29 +0800

    华为机试： 1.放苹果 2.201301 JAVA 题目2-3级 3.最小公倍数（未解）

---
+++ algorithmDemo/src/com/lqb/algorithm/hauwei/LeastCommonMultiple.java
@@ -0,0 +1,37 @@
+package com.lqb.algorithm.hauwei;
+
+/**
+ * 正整数A和正整数B 的最小公倍数是指 能被A和B整除的最小的正整数值，设计一个算法，求输入A和B的最小公倍数。
+ * 
+ * 输入描述:
+ * 输入两个正整数A和B。
+ * 输出描述:
+ * 输出A和B的最小公倍数。
+ * 
+ * 输入例子:
+ * 5 7
+ * 输出例子:
+ * 35
+ * 
+ * @Author:JackBauer
+ * @Date:2016年8月2日
+ */
+public class LeastCommonMultiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int test(int n, int m) {
+		
+		
+		
+		
+		
+		
+		
+		return 0;
+	}
+
+}
