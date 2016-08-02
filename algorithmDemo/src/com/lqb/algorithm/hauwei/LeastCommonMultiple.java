9fb9e13d93d6ff8e1db63f64939beda2b93ae6c1
cable5881 <412771478@qq.com>
2016-08-02 13:30:47 +0800

    华为机试：最大公倍数

--- algorithmDemo/src/com/lqb/algorithm/hauwei/LeastCommonMultiple.java
+++ algorithmDemo/src/com/lqb/algorithm/hauwei/LeastCommonMultiple.java
@@ -1,5 +1,7 @@
 package com.lqb.algorithm.hauwei;
 
+import java.util.Scanner;
+
 /**
  * 正整数A和正整数B 的最小公倍数是指 能被A和B整除的最小的正整数值，设计一个算法，求输入A和B的最小公倍数。
  * 
@@ -13,25 +15,46 @@ package com.lqb.algorithm.hauwei;
  * 输出例子:
  * 35
  * 
+ * 最小公倍数 = 两数之积除以最大公约数
+ * 
  * @Author:JackBauer
  * @Date:2016年8月2日
  */
 public class LeastCommonMultiple {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LeastCommonMultiple test = new LeastCommonMultiple();
+		Scanner input = new Scanner(System.in);
+		while(input.hasNext()){
+			int n = input.nextInt();
+			int m = input.nextInt();
+			int t = test.fun(n, m);
+			System.out.println(n * m / t);
+		}
+		
+		input.close();
 	}
 
-	public int test(int n, int m) {
+	public int fun(int n, int m) {
+		int big;
+		int small;
+		int temp;
 		
+		if(n > m){
+			big = n;
+			small = m;
+		}else{
+			big = m;
+			small = n;
+		}
 		
+		while(small > 0){
+			temp = big % small;
+			big = small;
+			small = temp;
+		}
 		
-		
-		
-		
-		
-		return 0;
+		return big;
 	}
 
 }
