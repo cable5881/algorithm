4cd1192ac41fdc0844e8fb207f2fceed8f5ee803
cable5881 <412771478@qq.com>
2016-08-05 14:01:55 +0800

    配置文件恢复

---
+++ algorithmDemo/src/com/lqb/algorithm/hauwei/MaximunContinousBitNum.java
@@ -0,0 +1,29 @@
+package com.lqb.algorithm.hauwei;
+
+/**
+ * 功能: 求一个byte数字对应的二进制数字中1的最大连续数，例如3的二进制为00000011，最大连续2个1   
+ * 输入: 一个byte型的数字   
+ * 输出: 无    
+ * 返回: 对应的二进制数字中1的最大连续数
+ * 
+ * 输入描述:
+ * 输入一个byte数字
+ * 输出描述:
+ * 输出转成二进制之后连续1的个数
+ * 
+ * 输入例子:
+ * 3
+ * 输出例子:
+ * 2
+ * 
+ * @Author:JackBauer
+ * @Date:2016年8月5日
+ */
+public class MaximunContinousBitNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
