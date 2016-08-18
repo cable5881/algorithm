b8c101986a68fb6b1ab27a096b5b20692ec76b29
cable5881 <412771478@qq.com>
2016-08-18 12:50:37 +0800

    整数转化

--- algorithmDemo/src/com/lqb/algorithm/cracking_the_coding_interview/BitSwapRequired.java
+++ algorithmDemo/src/com/lqb/algorithm/cracking_the_coding_interview/BitSwapRequired.java
@@ -15,7 +15,7 @@ package com.lqb.algorithm.cracking_the_coding_interview;
 public class BitSwapRequired {
 
 	public static void main(String[] args) {
-		System.out.println(calcCost(10, 5));
+		System.out.println(calcCost2(10, 5));
 
 	}
 
@@ -25,6 +25,15 @@ public class BitSwapRequired {
 			if ((c & 1) != 0) {
 				count++;
 			}
+		}
+
+		return count;
+	}
+
+	public static int calcCost2(int A, int B) {
+		int count = 0;
+		for (int c = A ^ B; c != 0; c = c & (c - 1)) {
+			count++;
 		}
 
 		return count;
