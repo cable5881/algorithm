51eecb5166db8949df87f88900065683922e444e
cable5881 <412771478@qq.com>
2016-10-13 20:24:24 +0800

    1.蚂蚁碰撞、二叉树找出下一个后继节点、二叉树公共祖先、上楼梯
    2.TreeNode更新

---
+++ algorithmDemo/src/com/lqb/algorithm/cracking_the_coding_interview/AntsCollision.java
@@ -0,0 +1,26 @@
+package com.lqb.algorithm.cracking_the_coding_interview;
+
+/**
+ * 在n个顶点的多边形上有n只蚂蚁，这些蚂蚁同时开始沿着多边形的边爬行，请求出这些蚂蚁相撞的概率。
+ * (这里的相撞是指存在任意两只蚂蚁会相撞)
+ * 给定一个int n(3<=n<=10000)，代表n边形和n只蚂蚁，请返回一个double，为相撞的概率。
+ * 
+ * @author:JackBauer
+ * @date:2016年10月13日
+ */
+public class AntsCollision {
+
+	public static void main(String[] args) {
+		AntsCollision test = new AntsCollision();
+		System.out.println(test.antsCollision(3));
+		System.out.println(test.antsCollision(4));
+		System.out.println(test.antsCollision(5));
+		System.out.println(test.antsCollision(10));
+		System.out.println(test.antsCollision(10000));
+	}
+	
+	public double antsCollision(int n) {
+		return 1 - Math.pow(0.5, n) * 2;
+    }
+
+}
