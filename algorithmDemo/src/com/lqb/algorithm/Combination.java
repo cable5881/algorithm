c710c4858e643c628ea2fa8b72e6e6bd97d356d8
cable5881 <412771478@qq.com>
2016-10-15 11:09:39 +0800

    组合思想

--- algorithmDemo/src/com/lqb/algorithm/Combination.java
+++ algorithmDemo/src/com/lqb/algorithm/Combination.java
@@ -10,12 +10,16 @@ import java.util.ArrayList;
  */
 public class Combination {
 
+	//分别调用两种方法
 	public static void main(String[] args) {
 		String s1 = "abcd";
 
-		ArrayList<String> combinations = getCombination(s1.toCharArray());
-		System.out.println(combinations);
-
+		ArrayList<String> combinations1 = getCombination(s1.toCharArray());
+		System.out.println(combinations1);
+		
+		ArrayList<String> combinations2 = new ArrayList<>();
+		getCombinationCore(s1.toCharArray(), combinations2, s1.length() - 1);
+		System.out.println(combinations2);
 	}
 
 	public static ArrayList<String> getCombination(char[] ch) {
@@ -26,24 +30,74 @@ public class Combination {
 
 		StringBuilder sb = new StringBuilder();
 		getCombinationCore(ch, combinations, sb, 0);
-
+		
 		return combinations;
 	}
 
+	/**
+	 * 核心思想
+	 * 
+	 	a
+	 		ab
+	 			abc
+	 				return
+	 		ac
+	 			return
+	 	b
+	 		bc
+	 			return
+	 	c
+	 * 
+	 * @author:JackBauer
+	 * @date:2016年10月15日
+	 * @param ch
+	 * @param combinations
+	 * @param sb
+	 * @param start
+	 */
 	private static void getCombinationCore(char[] ch, ArrayList<String> combinations, StringBuilder sb, int start) {
 		if (start >= ch.length) {
 			return;
 		}
 
-		for (int i = start, j = 0; i < ch.length; i++, j++) {
+		for (int i = start; i < ch.length; i++) {
 			sb.append(ch[i]);
 			combinations.add(sb.toString());
 
-			getCombinationCore(ch, combinations, sb, start + j + 1);
+			getCombinationCore(ch, combinations, sb, i + 1);
 
 			sb.deleteCharAt(sb.length() - 1);
 		}
 
+	}
+	
+	/**
+	 * 核心思想 : f(n)的值为 f(n - 1)的值加上n与f(n - 1)合并的值再加上n
+	 * 
+	 * f(a) = a => [a]
+	 * f(ab) = f(a) + b(a) + b => [a, ab, b]
+	 * f(abc) = f(ab) + cf(ab) + c => [a, ab, b, ac, abc, bc, c]
+	 * 
+	 * 只要找到f(abc) 与 f(ab) 的关系即可，即问题与子问题的关系
+	 * 
+	 * @author:JackBauer
+	 * @date:2016年10月15日
+	 * @param ch
+	 * @param combinations
+	 * @param start
+	 */
+	private static void getCombinationCore(char[] ch, ArrayList<String> combinations, int start) {
+		if (start < 0) {
+			return;
+		}
+		
+		getCombinationCore(ch, combinations, start - 1);
+		
+		for(String s : combinations.toArray(new String[combinations.size()])) {
+			combinations.add(s + ch[start]);
+		}
+		
+		combinations.add(ch[start] + "");
 	}
 
 }
