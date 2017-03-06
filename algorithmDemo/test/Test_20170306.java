6e2eff289d91d1efe0fa10cb4b7aedf1c5904813
cable5881 <412771478@qq.com>
2017-03-06 17:35:33 +0800

    增加了测试目录

---
+++ algorithmDemo/test/Test_20170306.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * Created by JackBauer on 2017/3/6.
+ */
+public class Test_20170306 {
+
+    public static void main(String[] args) {
+        System.out.println((int)'0'); // 48
+        System.out.println((int)'a'); // 97
+        System.out.println((int)'A'); // 65
+    }
+
+}
