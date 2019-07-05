50e73794abc22b9f1af6da5e749c1638c541e7f2
liqibo <bauer.li@weshareholdings.com>
2019-07-05 16:51:08 +0800

    FindPeakElement

--- algorithmDemo/test/Test_20170306.java
+++ algorithmDemo/test/Test_20170306.java
@@ -9,6 +9,10 @@ public class Test_20170306 {
         System.out.println((int)'0'); // 48
         System.out.println((int)'a'); // 97
         System.out.println((int)'A'); // 65
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE / 10);
+        System.out.println(Integer.MAX_VALUE / 10 + 7);
+        System.out.println(Math.pow(2, 16));
     }
 
 }
