2be010be769c70f00e832232c32f60c674422460
liqibo <bauer.li@weshareholdings.com>
2019-10-31 11:39:35 +0800

    trie

---
+++ src/com/lqb/leetcode/SuperEggDrop.java
@@ -0,0 +1,202 @@
+package com.lqb.leetcode;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 你将获得 K 个鸡蛋，并可以使用一栋从 1 到 N  共有 N 层楼的建筑。
+ * 每个蛋的功能都是一样的，如果一个蛋碎了，你就不能再把它掉下去。
+ * 你知道存在楼层 F ，满足 0 <= F <= N 任何从高于 F 的楼层落下的鸡蛋都会碎，从 F 楼层或比它低的楼层落下的鸡蛋都不会破。
+ * 每次移动，你可以取一个鸡蛋（如果你有完整的鸡蛋）并把它从任一楼层 X 扔下（满足 1 <= X <= N）。
+ * 你的目标是确切地知道 F 的值是多少。
+ * 无论 F 的初始值如何，你确定 F 的值的最小移动次数是多少？
+ *
+ * 示例 1：
+ * 输入：K = 1, N = 2
+ * 输出：2
+ * 解释：
+ * 鸡蛋从 1 楼掉落。如果它碎了，我们肯定知道 F = 0 。
+ * 否则，鸡蛋从 2 楼掉落。如果它碎了，我们肯定知道 F = 1 。
+ * 如果它没碎，那么我们肯定知道 F = 2 。
+ * 因此，在最坏的情况下我们需要移动 2 次以确定 F 是多少。
+ *
+ * 示例 2：
+ * 输入：K = 2, N = 6
+ * 输出：3
+ *
+ * 示例 3：
+ * 输入：K = 3, N = 14
+ * 输出：4
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= K <= 100
+ * 1 <= N <= 10000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/super-egg-drop
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class SuperEggDrop {
+
+    @Test
+    public void test() {
+        System.out.println(superEggDrop2(1, 2));//2
+        System.out.println(superEggDrop2(1, 3));//3
+        System.out.println(superEggDrop2(2, 6));//3
+        System.out.println(superEggDrop2(2, 7));//4
+        System.out.println(superEggDrop2(3, 14));//4
+        System.out.println(superEggDrop2(3, 26));//6
+        System.out.println(superEggDrop2(4, 10000));//23
+        System.out.println(superEggDrop2(10, 10000));//14
+    }
+
+    //错误解法
+    public int superEggDrop(int K, int N) {
+        if (N == 2) {
+            return 2;
+        } else if (N == 1) {
+            return 1;
+        } else if (N == 0) {
+            return 0;
+        }
+
+        return superEggDrop(K - 1, (N + 1) / 2) + 1;
+    }
+
+    /**
+     * 暴力法
+     */
+    public int superEggDrop1(int K, int N) {
+        if (N <= 1 || K == 1) {
+            return N;
+        }
+
+        int min = N;
+        //从1楼开始丢鸡蛋丢到N楼
+        for (int i = 1; i <= N; i++) {
+            //如果i楼鸡蛋破了则往下一层楼看看，如果没破则上一层看看
+            //求出至少需要移动的次数
+            int tmp = Math.max(superEggDrop1(K - 1, i - 1), superEggDrop1(K, N - i));
+            //然后取最小
+            min = Math.min(tmp + 1, min);
+        }
+
+        return min;
+    }
+
+    /**
+     * 动态规划法0：使用一个二维数组来记录不同的K和N下至少需要移动的次数
+     * （空间复杂度O(KN)），使得时间复杂度降低到了O(KN^2)
+     */
+    public int superEggDrop2(int K, int N) {
+        int[][] middleResults = new int[K + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            // 只有1个鸡蛋的情况
+            middleResults[1][i] = i;
+            // 没有鸡蛋的情况
+            middleResults[0][i] = 0;
+        }
+        //for (int i = 1; i <= K; i++) {
+        //    // 0层大楼
+        //    middleResults[i][0] = 0;
+        //}
+
+        //可以看到这里没有用递归，因此要计算所有情况
+        // 从2个鸡蛋开始
+        for (int k = 2; k <= K; k++) {
+            for (int n = 1; n <= N; n++) {
+                int tMinDrop = Integer.MAX_VALUE;
+                //n层高的大楼，鸡蛋从1~n开始扔
+                for (int x = 1; x <= n; x++) {
+                    tMinDrop = Math.min(tMinDrop, 1 + Math.max(middleResults[k - 1][x - 1], middleResults[k][n - x]));
+                }
+
+                //到这里我们就能得出n层楼下的至少移动次数
+                middleResults[k][n] = tMinDrop;
+            }
+        }
+
+        return middleResults[K][N];
+    }
+
+    Map<Integer, Integer> dp = new HashMap();
+    /**
+     * 动态规划法1：使用一个Map来记录不同的K和N下至少需要移动的次数
+     * 在测试用例4, 10000下会栈溢出
+     */
+    public int superEggDrop3(int K, int N) {
+        int key = getKey(K, N);
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
+
+        int ans;
+        if (N <= 1 || K == 1) {
+            ans = N;
+        } else {
+            ans = N;
+            //从1楼开始丢鸡蛋丢到N楼
+            for (int i = 1; i <= N; i++) {
+                //如果i楼鸡蛋破了则往下一层楼看看，如果没破则上一层看看
+                //求出至少需要移动的次数
+                int tmp = Math.max(superEggDrop3(K - 1, i - 1), superEggDrop3(K, N - i));
+                //然后取最小
+                ans = Math.min(tmp + 1, ans);
+            }
+
+        }
+
+        dp.put(key, ans);
+        return dp.get(key);
+    }
+    private int getKey(int K, int N) {
+        //N * 100 + K 是用来将N和K合并成一个KEY的
+        return N * 100 + K;
+    }
+
+    /**
+     * 动态规划法2：优化，不用每一层都去尝试丢鸡蛋，使用二分法减少递归次数
+     */
+    public int superEggDrop4(int K, int N) {
+        int key = getKey(K, N);
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
+
+        int ans = 0;
+        if (N <= 1 || K == 1) {
+            ans = N;
+        } else {
+
+            int l = 1;
+            int r = N;
+            int m;
+            ans = N;
+
+            while (l < r) {
+                m = (r - l) / 2 + l;
+                int t1 = superEggDrop4(K - 1, m - 1);
+                int t2 = superEggDrop4(K, N - m);
+
+                if (t1 > t2) {
+                    l = m;
+                } else if (t1 < t2) {
+                    r = m;
+                } else {
+                    l = r = m;
+                }
+
+            }
+
+            //ans = Math.min(ans, superEggDrop2())
+
+        }
+
+        dp.put(key, ans);
+        return dp.get(key);
+    }
+}
---
+++ src/com/lqb/leetcode/Trie.java
@@ -0,0 +1,101 @@
+package com.lqb.leetcode;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+ *
+ * 示例:
+ *
+ * Trie trie = new Trie();
+ *
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 true
+ * trie.search("app");     // 返回 false
+ * trie.startsWith("app"); // 返回 true
+ * trie.insert("app");
+ * trie.search("app");     // 返回 true
+ * 说明:
+ *
+ * 你可以假设所有的输入都是由小写字母 a-z 构成的。
+ * 保证所有输入均为非空字符串。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class Trie {
+
+    @Test
+    public void test() {
+        Trie t = new Trie();
+        t.insert("apple");
+        System.out.println(t.search("apple"));
+        System.out.println(t.search("app"));
+        System.out.println(t.startsWith("app"));
+        t.insert("app");
+        System.out.println(t.search("app"));
+    }
+
+    /**由于题目规定了字符串中只包含26个字母，因此这里可以优化成一个26长度的数组*/
+    private Map<Character, Trie> links;
+
+    /**用来标记到目前为止是不是一个单词，如trie树中插入“apple”，此时搜索到了“app”它不是一个单词*/
+    private boolean isWord;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        this.links = new HashMap<>();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        if (word == null || word.equals("")) {
+            return;
+        }
+
+        Trie t = this;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (t.links.containsKey(c)) {
+                t = t.links.get(c);
+                continue;
+            }
+
+            Trie temp = new Trie();
+            t.links.put(c, temp);
+            t = temp;
+        }
+
+        t.isWord = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Trie trie = searchTrie(word);
+        return trie != null && trie.isWord;
+    }
+
+    private Trie searchTrie(String word) {
+        Trie t = this;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!t.links.containsKey(c)) {
+                return null;
+            }
+
+            t = t.links.get(c);
+        }
+
+        return t;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        return searchTrie(prefix) != null;
+    }
+
+}
