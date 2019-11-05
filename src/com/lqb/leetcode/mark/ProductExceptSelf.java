b02e72869712a463909bb96ac5cd3ad95a283b6f
liqibo <bauer.li@weshareholdings.com>
2019-11-05 12:25:51 +0800

    除自身以外数组的乘积

--- src/com/lqb/leetcode/mark/ProductExceptSelf.java
+++ src/com/lqb/leetcode/mark/ProductExceptSelf.java
@@ -1,11 +1,12 @@
-package com.lqb.leetcode;
+package com.lqb.leetcode.mark;
 
 import org.junit.Test;
 
 import java.util.Arrays;
 
 /**
- * 给定长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
+ * 给定长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，
+ * 其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
  * 输入: [1,2,3,4]
  * 输出: [24,12,8,6]
  * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
