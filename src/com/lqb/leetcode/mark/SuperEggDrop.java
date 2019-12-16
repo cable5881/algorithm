f9048492181ae197b22f6ba67ecd1fa49253d5eb
liqibo <bauer.li@weshareholdings.com>
2019-12-16 20:03:32 +0800

    丢鸡蛋

--- src/com/lqb/leetcode/mark/SuperEggDrop.java
+++ src/com/lqb/leetcode/mark/SuperEggDrop.java
@@ -44,13 +44,13 @@ public class SuperEggDrop {
 
     @Test
     public void test() {
-        System.out.println(superEggDrop_w2(1, 2));//2
-        System.out.println(superEggDrop_w2(1, 3));//3
-        System.out.println(superEggDrop_w2(2, 6));//3
-        System.out.println(superEggDrop_w2(2, 7));//4
-        System.out.println(superEggDrop_w2(3, 14));//4
-        System.out.println(superEggDrop_w2(3, 26));//6
-        //System.out.println(superEggDrop_w2(4, 10000));//23
+        System.out.println(superEggDrop4(1, 2));//2
+        System.out.println(superEggDrop4(1, 3));//3
+        System.out.println(superEggDrop4(2, 6));//3
+        System.out.println(superEggDrop4(2, 7));//4
+        System.out.println(superEggDrop4(3, 14));//4
+        System.out.println(superEggDrop4(3, 26));//6
+        System.out.println(superEggDrop4(4, 10000));//23
         //System.out.println(superEggDrop_w2(10, 10000));//14
     }
 
@@ -70,8 +70,15 @@ public class SuperEggDrop {
     /**
      * @author liqibo
      * @date 2019/11/21 16:13
-     * @description 错误解法2
-     * 往上走或往下走，每次走剩下楼层的 1/2层
+     * @description 错误解法2: 往上走或往下走，每次走剩下楼层的 1/2层...
+     * 如果不限制鸡蛋个数的话，这种二分思路可以得到最少尝试的次数
+     * 那干脆先用二分查找，等到只剩 1 个鸡蛋的时候再执行线性扫描，这样得到的结果是不是就是最少的扔鸡蛋次数呢？
+     * 很遗憾，并不是，比如说把楼层变高一些，100 层，给你 2 个鸡蛋，你在 50 层扔一下，碎了，那就只能线性扫描 1～49 层了，最坏情况下要扔 50 次。
+     * 如果不要「二分」，变成「五分」「十分」都会大幅减少最坏情况下的尝试次数。
+     * 比方说第一个鸡蛋每隔十层楼扔，在哪里碎了第二个鸡蛋一个个线性扫描，总共不会超过 20 次​。
+     * 最优解其实是 14 次。最优策略非常多，而且并没有什么规律可言。没有什么规律可言。没有什么规律可言。没有什么规律可言。
+     * 没有什么规律可言。没有什么规律可言。没有什么规律可言。没有什么规律可言。没有什么规律可言。
+     * 没有什么规律可言。没有什么规律可言。没有什么规律可言。没有什么规律可言。没有什么规律可言。
      */
     public int superEggDrop_w2(int K, int N) {
         return superEggDrop_w2(K, 1, N);
@@ -99,24 +106,91 @@ public class SuperEggDrop {
             return N;
         }
 
+        //最大就是N
         int min = N;
+
         //从1楼开始丢鸡蛋丢到N楼
         for (int i = 1; i <= N; i++) {
             //如果i楼鸡蛋破了则往下一层楼看看，如果没破则上一层看看
             //求出至少需要移动的次数
             int tmp = Math.max(superEggDrop1(K - 1, i - 1), superEggDrop1(K, N - i));
-            //然后取最小
+
+            //到这里已经是进行完了，已经得到了从i楼开始丢鸡蛋的最坏次数
+            //现在需要将第i楼丢鸡蛋和其他楼丢鸡蛋对比然后取最小
+            //tmp+1容易忘记
             min = Math.min(tmp + 1, min);
         }
 
         return min;
     }
 
+
+    Map<Integer, Integer> dp = new HashMap();
+    /**
+     * 动态规划法1：使用一个Map来记录不同的K和N下至少需要移动的次数
+     * 在测试用例4, 10000下会栈溢出
+     */
+    public int superEggDrop3(int K, int N) {
+        //N * 100 + K 是用来将N和K合并成一个KEY的
+        int key = N * 100 + K;
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
+
+        int ans = N;
+        if (N > 1 && K > 1) {
+            for (int i = 1; i <= N; i++) {
+                int tmp = Math.max(superEggDrop3(K - 1, i - 1), superEggDrop3(K, N - i));
+                ans = Math.min(tmp + 1, ans);
+            }
+        }
+
+        dp.put(key, ans);
+        return dp.get(key);
+    }
+
+    /**
+     * 动态规划法2：优化，不用每一层都去尝试丢鸡蛋，使用二分法减少递归次数
+     */
+    public int superEggDrop4(int K, int N) {
+        int key = N * 100 + K;
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
+
+        int ans = 0;
+        if (N <= 1 || K == 1) {
+            ans = N;
+        } else {
+            int l = 1;
+            int r = N;
+            ans = N;
+
+            while (l <= r) {
+                int m = (l + r) / 2;
+                int broken = superEggDrop4(K - 1, m - 1);
+                int notBroken  = superEggDrop4(K, N - m);
+
+                if (broken > notBroken ) {
+                    r = m - 1;
+                    ans = Math.max(ans, broken + 1);
+                } else {
+                    l = m + 1;
+                    ans = Math.max(ans, notBroken + 1);
+                }
+            }
+        }
+
+        dp.put(key, ans);
+        return ans;
+    }
+
     /**
      * 动态规划法0：使用一个二维数组来记录不同的K和N下至少需要移动的次数
      * （空间复杂度O(KN)），使得时间复杂度降低到了O(KN^2)
      */
-    public int superEggDrop2(int K, int N) {
+    public int superEggDrop5(int K, int N) {
+        //可以看到这里没有用递归，因此要计算所有情况
         int[][] middleResults = new int[K + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             // 只有1个鸡蛋的情况
@@ -129,7 +203,6 @@ public class SuperEggDrop {
         //    middleResults[i][0] = 0;
         //}
 
-        //可以看到这里没有用递归，因此要计算所有情况
         // 从2个鸡蛋开始
         for (int k = 2; k <= K; k++) {
             for (int n = 1; n <= N; n++) {
@@ -145,83 +218,6 @@ public class SuperEggDrop {
         }
 
         return middleResults[K][N];
-    }
-
-    Map<Integer, Integer> dp = new HashMap();
-    /**
-     * 动态规划法1：使用一个Map来记录不同的K和N下至少需要移动的次数
-     * 在测试用例4, 10000下会栈溢出
-     */
-    public int superEggDrop3(int K, int N) {
-        int key = getKey(K, N);
-        if (dp.containsKey(key)) {
-            return dp.get(key);
-        }
-
-        int ans;
-        if (N <= 1 || K == 1) {
-            ans = N;
-        } else {
-            ans = N;
-            //从1楼开始丢鸡蛋丢到N楼
-            for (int i = 1; i <= N; i++) {
-                //如果i楼鸡蛋破了则往下一层楼看看，如果没破则上一层看看
-                //求出至少需要移动的次数
-                int tmp = Math.max(superEggDrop3(K - 1, i - 1), superEggDrop3(K, N - i));
-                //然后取最小
-                ans = Math.min(tmp + 1, ans);
-            }
-
-        }
-
-        dp.put(key, ans);
-        return dp.get(key);
-    }
-    private int getKey(int K, int N) {
-        //N * 100 + K 是用来将N和K合并成一个KEY的
-        return N * 100 + K;
-    }
-
-    /**
-     * 动态规划法2：优化，不用每一层都去尝试丢鸡蛋，使用二分法减少递归次数
-     */
-    public int superEggDrop4(int K, int N) {
-        int key = getKey(K, N);
-        if (dp.containsKey(key)) {
-            return dp.get(key);
-        }
-
-        int ans = 0;
-        if (N <= 1 || K == 1) {
-            ans = N;
-        } else {
-
-            int l = 1;
-            int r = N;
-            int m;
-            ans = N;
-
-            while (l < r) {
-                m = (r - l) / 2 + l;
-                int t1 = superEggDrop4(K - 1, m - 1);
-                int t2 = superEggDrop4(K, N - m);
-
-                if (t1 > t2) {
-                    l = m;
-                } else if (t1 < t2) {
-                    r = m;
-                } else {
-                    l = r = m;
-                }
-
-            }
-
-            //ans = Math.min(ans, superEggDrop2())
-
-        }
-
-        dp.put(key, ans);
-        return dp.get(key);
     }
 
 }
