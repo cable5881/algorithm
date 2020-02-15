bbc29bb8c3062ef384a2018581eceaf8e3496540
cable5881 <412771478@qq.com>
2020-02-15 20:22:35 +0800

    更新指数、打印最大的n位数

--- src/com/lqb/offer/NumberOf1.java
+++ src/com/lqb/offer/NumberOf1.java
@@ -2,22 +2,21 @@ package com.lqb.offer;
 
 public class NumberOf1 {
 
-	public static void main(String[] args) {
-		System.out.println(Math.abs(-219999999));
-		System.out.println( numberOf1(-229999999) );
+    public static void main(String[] args) {
+        System.out.println(numberOf1(-1));
 
-	}
-	
-	public static int numberOf1(int n) {
-        
-		int count = 0;
-		
-		while( n != 0 ){
-			count++;
-			n = (n - 1) & n;
-		}
-		
-		return count;
+    }
+
+    public static int numberOf1(int n) {
+
+        int count = 0;
+
+        while (n != 0) {
+            count++;
+            n = (n - 1) & n;
+        }
+
+        return count;
     }
 
 }
