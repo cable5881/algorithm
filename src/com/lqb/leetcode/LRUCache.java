fe87234a2f9112657ba134e552b91b7fc9520348
liqibo <bauer.li@weshareholdings.com>
2020-03-02 19:55:34 +0800

    LRU增加继承LinkedHashMap的形式

--- src/com/lqb/leetcode/LRUCache.java
+++ src/com/lqb/leetcode/LRUCache.java
@@ -301,16 +301,25 @@ class LRUCache1 {
  * @date 2020/3/2 16:35
  * @description 继承LinkedHashMap
  */
-class LRUCache2 extends LinkedHashMap {
+class LRUCache2 {
 
-    private int capacity;
+    private Map<Integer, Integer> cache;
 
     public LRUCache2(int capacity) {
-        this.capacity = capacity;
+        //注意要按照访问顺序
+        this.cache = new LinkedHashMap(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return this.size() > capacity;
+            }
+        };
     }
 
-    @Override
-    protected boolean removeEldestEntry(Map.Entry eldest) {
-        return this.size() > capacity;
+    public int get(int key) {
+        return this.cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        this.cache.put(key, value);
     }
 }
