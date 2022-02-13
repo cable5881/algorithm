3c7586da3af9d6409975f7c4e0021747c031b7f6
mambali <mambali@tencent.com>
2022-02-14 00:14:21 +0800

    翻转二叉树

---
+++ src/com/lqb/leetcode/InvertBinaryTree.java
@@ -0,0 +1,21 @@
+package com.lqb.leetcode;
+
+import com.lqb.util.TreeNode;
+
+/**
+ * 翻转二叉树
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
