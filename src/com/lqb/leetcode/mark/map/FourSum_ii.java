0079b6ff5076fee3d2c8064313bb81aac2551476
mambali <mambali@tencent.com>
2022-02-13 23:18:24 +0800

    四数相加 II

---
+++ src/com/lqb/leetcode/mark/map/FourSum_ii.java
@@ -0,0 +1,59 @@
+package com.lqb.leetcode.mark.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+/**
+ * 四数相加 II
+ * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+ *
+ * 0 <= i, j, k, l < n
+ * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ *  
+ *
+ * 示例 1：
+ * 输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+ * 输出：2
+ * 解释：
+ * 两个元组如下：
+ * 1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+ * 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+ *
+ * 示例 2：
+ * 输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+ * 输出：1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/4sum-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FourSum_ii {
+
+    @Test
+    public void test() {
+        System.out.println(fourSumCount(new int[]{1,2}, new int[]{-2,-1}, new int[]{-1,2}, new int[]{0,2}));
+        System.out.println(fourSumCount(new int[]{0}, new int[]{0}, new int[]{0}, new int[]{0}));
+    }
+
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int res = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                int twoSum = nums1[i] + nums2[j];
+                Integer cnt = map.getOrDefault(twoSum, 0);
+                map.put(twoSum, ++cnt);
+            }
+        }
+
+        for (int i = 0; i < nums3.length; i++) {
+            for (int j = 0; j < nums4.length; j++) {
+                int twoSum = -(nums3[i] + nums4[j]);
+                Integer cnt = map.getOrDefault(twoSum, 0);
+                res += cnt;
+            }
+        }
+        return res;
+    }
+}
